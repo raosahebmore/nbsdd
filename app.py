03bc0d86b735ad6d0d936272cea4cbc95f41dcaf
raosahebmore <42617461+raosahebmore@users.noreply.github.com>
2019-02-22 22:49:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,5 @@
 
 
 import pandas as pd
-import numpy as np
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.dependencies import Input, Output
-from plotly import graph_objs as go
-N = 100000
 
-
-app = dash.Dash()
-app.config['suppress_callback_exceptions']=True
-
-
-app.layout = html.Div(
-    html.H1(children='Hello Dash'))
-
-if __name__ == '__main__':
-    app.run_server()
+print("Hellow World"!)
